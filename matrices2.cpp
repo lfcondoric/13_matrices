1659bce0e03a5fedc63f1010ae438314ac9d58f4
lfcondoric <lfcondoric@unjbg.edu.pe>
2024-07-15 10:37:46 -0500

    Añadiendo mensajes a matrices 2

--- matrices2.cpp
+++ matrices2.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 int main(){
 	int n,matriz[50][50],s=0,a=0;
-	cin>>n;
+	cout<<"Ingrese el tamaño de la matriz: ";cin>>n;
 	for(int i=0;i<n;i++){
+		cout<<"Ingrese los elementos de la fila "<<i+1<<": \n";
 		for(int j=0;j<n;j++){
 			cin>>matriz[i][j];
 		}
@@ -11,7 +12,7 @@ int main(){
 	for(int i=0;i<n;i++){
 			s+=matriz[i][n-i-1];
 	}
-	cout<<s;
+	cout<<"La suma de la diagonal secundaria es: "<<s;
 	
 	return 0;
 }
