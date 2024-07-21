131064f836b6cdef31e99e442c07bcaf0a7a9ee7
lfcondoric <lfcondoric@unjbg.edu.pe>
2024-07-20 21:51:34 -0500

    Actualización matrices3

--- matrices3.cpp
+++ matrices3.cpp
@@ -6,13 +6,14 @@ int main(){
 	cout<<"Ingrese la filas de la matriz: ";cin>>n;
 	for(int j=0;j<3*n;j++){
 	matriz[0][j]=1;
-	}
-	for(int j=0;j<3*n;j++){
 	matriz[n-1][j]=1;
 	}
-	for(int i=2;i<n-1;i++){
-		for(int j=0;j<3*n;j++){
-	matriz[n-i][j]=0;	
+	
+	for(int i=2;i<n;i++){
+		for(int j=0;j<(3*n);j++){
+	matriz[n-i][j]=0;
+	matriz[n-i][0]=1;
+	matriz[n-i][(3*n)-1]=1;
 		}
 	}
 	for(int i=0;i<n;i++){
